fc7f25ec153fe04053317d879d73a169309f88d0
PepeAliasJose <peperc781@gmail.com>
2023-12-05 12:25:03 +0100

    Permutations - CSES 005

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - Missing Number    - 2023-11-29 00:39:22	C++	0.04 s	615 ch.
 - Repetitions       - 2023-11-29 10:17:55	C++	0.03 s	303 ch.
 - Increasing Array  - 2023-12-04 15:03:26	C++	0.06 s	590 ch.
+- Permutations      - 2023-12-05 13:20:07	C++	0.08 s	214 ch.
---
+++ cses005/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    /*
+    If the synchronization is turned off, the C++ standard streams are allowed to buffer their I/O independently, 
+    which may be considerably faster in some cases.
+    */
+    int num; cin >> num;
+
+    if(num>3 || num==1){
+        for(int x = 2; x<=num; x+=2){
+            cout << x << " ";
+        }
+        for(int x = 1; x<=num; x+=2){
+            cout << x << " ";
+        }
+    }else{
+        cout << "NO SOLUTION";
+    }
+}
